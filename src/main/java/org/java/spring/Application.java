554b1ace777ca619378f3c743686f8041262524c
Gioele Iacovella <g.ioele96@outlook.it>
2023-12-07 15:27:35 +0100

    added DiscountController

--- src/main/java/org/java/spring/Application.java
+++ src/main/java/org/java/spring/Application.java
@@ -1,6 +1,11 @@
 package org.java.spring;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.java.spring.db.pojo.Discount;
 import org.java.spring.db.pojo.Pizza;
+import org.java.spring.db.serv.DiscountService;
 import org.java.spring.db.serv.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,6 +19,9 @@ public class Application implements CommandLineRunner {
 	@Autowired
 	private PizzaService pizzaService;
 	
+	@Autowired
+	private DiscountService discountService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
@@ -25,5 +33,14 @@ public class Application implements CommandLineRunner {
         pizzaService.save(new Pizza("Quattro Formaggi", "Pizza con quattro tipi di formaggio", "url_quattro_formaggi", 10.99f));
         pizzaService.save(new Pizza("Vegetariana", "Pizza vegetariana con verdure fresche", "url_vegetariana", 9.49f));
         pizzaService.save(new Pizza("Capricciosa", "Pizza con prosciutto, funghi e carciofi", "url_capricciosa", 11.49f));
+        
+        List<Pizza> pizzas = pizzaService.findAll();
+        
+        for (Pizza pizza : pizzas) {
+            LocalDate startDate = LocalDate.now();
+            LocalDate endDate = startDate.plusDays(7);
+            Discount discount = new Discount(startDate, endDate, "Sconto " + pizza.getName(), pizza);
+            discountService.save(discount);
+        }
 	}
 }
